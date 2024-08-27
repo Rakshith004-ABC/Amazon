d981613d8c0d6740cca69864626f322a95ca7487
Rakshith N <rakshithramesh@yahoo.in>
2024-08-27 20:36:51 +0530

    Update App.java-Rak

--- Amazon/Amazon-Core/src/main/java/org/amazon/App.java
+++ Amazon/Amazon-Core/src/main/java/org/amazon/App.java
@@ -1,13 +1,8 @@
 package org.amazon;
 
 /**
+ * Hello world- Sample java program
  * Hello world
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
+public class App
